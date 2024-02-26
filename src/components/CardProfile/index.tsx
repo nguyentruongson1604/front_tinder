4cee5eef20d35290c803aada79b10fa1ca30fcdb
MTT SON NGUYEN <mtt@son-nguyen.local>
2024-02-26 18:29:32 +0700

    switch/profile

---
+++ src/components/CardProfile/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {ImageBackground, StyleSheet, Text, View} from 'react-native';
+
+const CardProfile = () => {
+  return (
+    <View style={styles.card}>
+      <ImageBackground
+        source={{
+          uri: 'https://assets.vogue.in/photos/640592409d03d0d41504f3a0/master/pass/Face%20taping%20.jpg',
+        }}
+        style={styles.image}>
+        <View style={styles.wrapContent}>
+          <Text style={styles.name}>Nguyen Truong Son</Text>
+          <Text style={styles.bio}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </Text>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    width: '90%',
+    height: '70%',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 7,
+    },
+    shadowOpacity: 0.41,
+    shadowRadius: 9.11,
+
+    elevation: 14,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    justifyContent: 'flex-end',
+  },
+  name: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  bio: {
+    color: 'white',
+    fontSize: 18,
+    lineHeight: 24,
+  },
+  wrapContent: {
+    padding: 10,
+  },
+});
+export default CardProfile;
